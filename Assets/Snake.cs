0f7def843b03ebc89f005ac1aa5b192dacbe3634
SMEAGOL\Adam <adno96@gmail.com>
2021-08-24 21:09:29 +0200

    Init snake proj

---
+++ Assets/Snake.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Snake : MonoBehaviour {
+
+	Vector2 dir = Vector2.right; //starting direction
+	Vector2 currentDir;
+
+	
+
+	List<Transform> tail = new List<Transform>(); // List of tail pieces
+
+	public bool ate = false; //did snek just ate?
+	public bool ateUpdate = false;
+
+	public GameObject tailPrefab; //assign tail prefab later
+
+	public float speed = 0.2f;
+
+	// Use this for initialization
+	void Start () {
+		// call "move" function every speed
+		//InvokeRepeating(Move(), speed, speed); old move update
+		StartCoroutine(MoveUpdate());
+		currentDir = dir;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.RightArrow) && currentDir != Vector2.left){
+			dir = Vector2.right;
+		} else if (Input.GetKey(KeyCode.DownArrow) && currentDir != Vector2.up){
+			dir = -Vector2.up;
+		} else if (Input.GetKey(KeyCode.LeftArrow)&& currentDir != Vector2.right){
+			dir = -Vector2.right;
+		} else if (Input.GetKey(KeyCode.UpArrow)&& currentDir != Vector2.down){
+			dir = Vector2.up;
+		}
+
+		if(Input.GetKeyDown(KeyCode.LeftShift)){
+			speed/=5;
+		}
+		if(Input.GetKeyUp(KeyCode.LeftShift)){
+			speed*=5;
+		}
+	}
+
+	void Move() {
+		// save current pos, (where gap is)
+		Vector2 v = transform.position;
+
+		transform.Translate(dir);
+
+		if(ate) {
+			GameObject g = (GameObject)Instantiate(tailPrefab, v, Quaternion.identity);
+
+			tail.Insert(0, g.transform);
+			ate = false;
+		}
+
+		if (tail.Count > 0) {
+			tail.Last().position = v;
+
+			tail.Insert(0, tail.Last());
+			tail.RemoveAt(tail.Count-1);
+		}
+		currentDir = dir;
+	}
+
+	IEnumerator MoveUpdate() {
+		for(;;){
+			Move();
+			yield return new WaitForSeconds(speed);
+		}
+
+	}
+
+	void OnTriggerEnter2D(Collider2D coll){
+		//food?
+		if (coll.name.StartsWith("FoodPrefab")) {
+			ate = true;
+			ateUpdate = true;
+			Destroy(coll.gameObject);
+		} else {
+			//TODO (lose screen)
+		}
+	}
+}
---
+++ Assets/SpawnFood.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFood : MonoBehaviour {
+
+	// Food Prefab
+	public GameObject foodPrefab;
+
+	public Snake snake;
+
+	// Borders
+	public Transform borderTop;
+	public Transform borderBottom;
+	public Transform borderLeft;
+	public Transform borderRight;
+
+
+
+	// Use this for initialization
+	void Start () {
+		Spawn();
+	}
+
+	void Update () {
+		if (snake.ateUpdate){
+			Spawn();
+			snake.ateUpdate = false;
+		}
+	}
+
+	public void Spawn() {
+		int x = (int)Random.Range(borderLeft.position.x+1, borderRight.position.x-1);
+
+		int y = (int)Random.Range(borderBottom.position.y+1, borderTop.position.y-1);
+
+		Instantiate(foodPrefab, new Vector2(x, y), Quaternion.identity); 
+
+	}
+}
